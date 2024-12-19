133c2cd12579a65ec2fe179d6fde87d4690eef5d
I-mikan-I <67881102+I-mikan-I@users.noreply.github.com>
2024-12-19 07:47:22 +0100

    Day19💫

---
+++ src/day19/solution.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(require "../utils.rkt")
+(define input (file->string (rpath "input.txt")))
+
+(define patterns (string-split (car (string-split input "\n\n")) ", " #:trim? #t))
+(define designs (string-split (cadr (string-split input "\n\n")) "\n"))
+
+(define cache (make-hash))
+(define (valid design)
+  (hash-ref! cache
+             design
+             (lambda ()
+               (printf "checking: ~a\n" design)
+               (if (non-empty-string? design)
+                   (let ([trimmed (for/list ([pattern patterns]
+                                             #:when (string-prefix? design pattern))
+                                    (string-trim design pattern #:right? #f))])
+                     (for/sum ([trimmed trimmed]) (valid trimmed)))
+                   1))))
+
+(for/sum ([design designs] [i (in-naturals)]) (println i) (valid design))
